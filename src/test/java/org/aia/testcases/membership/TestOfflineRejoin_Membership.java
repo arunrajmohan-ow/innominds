4526f4daafbde437667b43de2540ce62929d7a4e
Suhas <sghodake@innominds.com>
2023-06-14 15:17:26 +0530

    Minor changes

--- src/test/java/org/aia/testcases/membership/TestOfflineRejoin_Membership.java
+++ src/test/java/org/aia/testcases/membership/TestOfflineRejoin_Membership.java
@@ -7,6 +7,9 @@ import org.aia.pages.api.MailinatorAPI;
 import org.aia.pages.api.membership.FontevaConnectionSOAP;
 import org.aia.pages.api.membership.JoinAPIValidation;
 import org.aia.pages.fonteva.membership.ContactCreateUser;
+import org.aia.pages.fonteva.membership.Memberships;
+import org.aia.pages.fonteva.membership.ReNewUser;
+import org.aia.pages.membership.DevSandBoxFonteva;
 import org.aia.utility.BrowserSetup;
 import org.aia.utility.ConfigDataProvider;
 import org.aia.utility.DataProviderFactory;
@@ -24,23 +27,25 @@ public class TestOfflineRejoin_Membership extends BaseClass {
 	ContactCreateUser fontevaJoin;
 	MailinatorAPI malinator;
 	JoinAPIValidation offlinApiValidation;
+	DevSandBoxFonteva fontevaPage;
 	public ExtentReports extent;
 	public ExtentTest extentTest;
-	
+
 	@BeforeMethod
 	public void setUp() throws Exception {
-		sessionID=new FontevaConnectionSOAP();
+		sessionID = new FontevaConnectionSOAP();
 		driver = BrowserSetup.startApplication(driver, DataProviderFactory.getConfig().getValue("browser"),
-				DataProviderFactory.getConfig().getValue("fontevaSessionIdUrl")+sessionID.getSessionID());
+				DataProviderFactory.getConfig().getValue("fontevaSessionIdUrl") + sessionID.getSessionID());
 		util = new Utility(driver, 30);
 		testData = new ConfigDataProvider();
 		fontevaJoin = PageFactory.initElements(driver, ContactCreateUser.class);
 		malinator = PageFactory.initElements(driver, MailinatorAPI.class);
+		fontevaPage = PageFactory.initElements(driver, DevSandBoxFonteva.class);
 		offlinApiValidation = PageFactory.initElements(driver, JoinAPIValidation.class);
 		// Configure Log4j to perform error logging
 		Logging.configure();
 	}
-	
+
 	@Test(priority = 1, description = "verify the offline membership rejoin in fonteva application(Admin flow)", enabled = true)
 	public void offlineReJoinProcess() throws InterruptedException {
 		ArrayList<String> dataList = fontevaJoin.userData();
@@ -51,11 +56,10 @@ public class TestOfflineRejoin_Membership extends BaseClass {
 		fontevaJoin.enterLicenseDetail();
 		fontevaJoin.createSalesOrder(testData.testDataProvider().getProperty("paymentMethod"));
 		fontevaJoin.applyPayment();
-		
-		
+		// Terminate created user
+		fontevaPage.changeTermDates(dataList.get(0)+" "+dataList.get(1));
 	}
-	
-	
+
 	@AfterMethod
 	public void teardown() {
 		BrowserSetup.closeBrowser(driver);
