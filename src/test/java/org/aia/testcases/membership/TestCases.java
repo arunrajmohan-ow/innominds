20ca07d7bfb2513067104aacec31da79ab93dd3e
Suhas <sghodake@innominds.com>
2023-06-20 13:41:32 +0530

    RereunTest

---
+++ src/test/java/org/aia/testcases/membership/TestCases.java
@@ -0,0 +1,18 @@
+package org.aia.testcases.membership;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+	@Test
+	public void test1() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test2() {
+		assertTrue(false);
+	}
+}
