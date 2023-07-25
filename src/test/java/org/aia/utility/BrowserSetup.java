aeb194e9e50f2549fa909fcce93ed13d579e484c
Suhas <sghodake@innominds.com>
2023-07-25 11:01:59 +0530

    Chrome

--- src/test/java/org/aia/utility/BrowserSetup.java
+++ src/test/java/org/aia/utility/BrowserSetup.java
@@ -26,7 +26,7 @@ public class BrowserSetup {
 
         if(browser.equalsIgnoreCase("Chrome")){
         	//System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
-        	//WebDriverManager.chromedriver().setup();
+        	WebDriverManager.chromedriver().setup();
         	Map<String, Object> pref = new HashMap<String, Object>();
     		pref.put("profile.default_content_settings.popups", false);
     		pref.put("autofill.profile_enabled", false);
@@ -37,14 +37,10 @@ public class BrowserSetup {
             options.addArguments("--ignore-certificate-errors");
             options.addArguments("--disable-notifications");
             options.addArguments("--remote-allow-origins=*");
-            /*options.addArguments("--headless"); //!!!should be enabled for Jenkins
+            options.addArguments("--headless"); //!!!should be enabled for Jenkins
             options.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
-            options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins*/
-			/*
-			 * DesiredCapabilities capabilities = new DesiredCapabilities();
-			 * capabilities.setBrowserName("Chrome");
-			 */
-             driver = new  RemoteWebDriver(new URL("https://10.50.17.45:5000"),options);
+            options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
+            //driver = new  RemoteWebDriver(new URL("https://10.50.17.45:5000"),options);
         }
         else if (browser.equalsIgnoreCase("firefox")) {
         	System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\Drivers\\geckodriver.exe");
