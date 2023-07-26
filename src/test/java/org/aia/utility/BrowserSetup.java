bb1b13498b8378f08448cf31fb35bf8b2d7bb887
Suhas <sghodake@innominds.com>
2023-07-26 17:05:58 +0530

    Webdriver

--- src/test/java/org/aia/utility/BrowserSetup.java
+++ src/test/java/org/aia/utility/BrowserSetup.java
@@ -19,6 +19,8 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class BrowserSetup {
 	
@@ -26,7 +28,7 @@ public class BrowserSetup {
 
         if(browser.equalsIgnoreCase("Chrome")){
         	//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
-        	//WebDriverManager.chromedriver().browserVersion("115.0.5790.102").setup();
+        	WebDriverManager.chromedriver().browserVersion("115.0.5790.102").forceDownload().setup();
         	//WebDriverManager.chromedriver().clearDriverCache().setup();
         	//System.out.println ("List of Drivers"+WebDriverManager.chromedriver().getDriverVersions());
         	Map<String, Object> pref = new HashMap<String, Object>();
