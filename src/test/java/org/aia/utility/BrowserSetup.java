d31cdc88f299e610827a9e766ee5df5a7c4164d9
Suhas <sghodake@innominds.com>
2023-07-26 17:24:26 +0530

    new browser

--- src/test/java/org/aia/utility/BrowserSetup.java
+++ src/test/java/org/aia/utility/BrowserSetup.java
@@ -27,8 +27,8 @@ public class BrowserSetup {
     public static WebDriver startApplication(WebDriver driver, String browser, String url) throws MalformedURLException{
 
         if(browser.equalsIgnoreCase("Chrome")){
-        	System.setProperty("webdriver.chrome.driver", "/home/jenkins/tools/chromedriver/chromedriver");
-        	//WebDriverManager.chromedriver().browserVersion("115.0.5790.102").forceDownload().setup();
+        	//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
+        	WebDriverManager.chromedriver().driverRepositoryUrl(new URL("https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/115.0.5790.102/linux64/chromedriver-linux64.zip")).forceDownload().setup();
         	//WebDriverManager.chromedriver().clearDriverCache().setup();
         	//System.out.println ("List of Drivers"+WebDriverManager.chromedriver().getDriverVersions());
         	Map<String, Object> pref = new HashMap<String, Object>();
