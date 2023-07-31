2c181e42f5ab062a66b3a29e5ac4d98082b0f08a
Suhas <sghodake@innominds.com>
2023-07-31 15:00:42 +0530

    Renew Changes

--- src/test/java/org/aia/pages/ces/RenewCESPage.java
+++ src/test/java/org/aia/pages/ces/RenewCESPage.java
@@ -58,7 +58,7 @@ public class RenewCESPage {
 	@FindBy(xpath="//button[text()='Next']") WebElement orgNextBtn;
 
 	public void renewMembership(String emaildata) throws InterruptedException {
-		Thread.sleep(20000);
+		Thread.sleep(50000);
 		util.waitUntilElement(driver, renewBtn);
 		renewBtn.click();
 
