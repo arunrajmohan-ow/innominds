487d1300c8182eb8cb60b28b9cd0f80466143f20
Suhas <sghodake@innominds.com>
2023-08-02 19:18:32 +0530

    Abs xpath

--- src/test/java/org/aia/pages/ces/RenewCESPage.java
+++ src/test/java/org/aia/pages/ces/RenewCESPage.java
@@ -34,8 +34,9 @@ public class RenewCESPage {
 	@FindBy(xpath = "//*[text()='What is your employee size?']")
 	WebElement empSizetxt;
 
-	@FindBy(xpath = "//span[@class='slds-checkbox--faux']")
+	//@FindBy(xpath = "//span[@class='slds-checkbox--faux']")
 	//@FindBy(xpath = "//div//label//span[@class='slds-checkbox--faux']")
+	@FindBy(xpath="/html[1]/body[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[1]/label[1]/span[1]")
 	WebElement agreeBtn;
 
 	@FindBy(xpath = "//button[@data-name='renewFormContinueBtn']")
