5ad0f10493d9d71c63821ede761d48617aff2075
Suhas <sghodake@innominds.com>
2023-08-02 12:08:12 +0530

    Browser

--- src/test/java/org/aia/utility/BrowserSetup.java
+++ src/test/java/org/aia/utility/BrowserSetup.java
@@ -86,7 +86,7 @@ public class BrowserSetup {
 		
 		System.out.println("LOG :Info- Browser Session getting terminated");
 
-		driver.quit();
+		//driver.quit();
 		
 		System.out.println("LOG :Info- Browser Session terminated");
 
