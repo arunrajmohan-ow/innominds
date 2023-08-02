48cb33c654c948e8edc37d16cb84844dffa7dbe6
Suhas <sghodake@innominds.com>
2023-08-02 09:43:21 +0530

    List of browsers

--- src/test/java/org/aia/utility/BrowserSetup.java
+++ src/test/java/org/aia/utility/BrowserSetup.java
@@ -25,6 +25,7 @@ public class BrowserSetup {
         if(browser.equalsIgnoreCase("Chrome")){
         	//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
         	WebDriverManager.chromedriver().setup();
+        	System.out.println(WebDriverManager.chromedriver().getWebDriverList());
         	//WebDriverManager.chromedriver().clearDriverCache().setup();
         	//System.out.println ("List of Drivers"+WebDriverManager.chromedriver().getDriverVersions());
         	Map<String, Object> pref = new HashMap<String, Object>();
