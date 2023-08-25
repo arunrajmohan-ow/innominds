e8e831187cd183b9de6be52dc569053cda64dfe5
Suhas <sghodake@innominds.com>
2023-08-25 09:56:33 +0530

    middel

--- src/test/java/org/aia/pages/fonteva/ces/CES_ContactPage.java
+++ src/test/java/org/aia/pages/fonteva/ces/CES_ContactPage.java
@@ -4,6 +4,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.aia.pages.fonteva.membership.ContactCreateUser;
 import org.aia.utility.ConfigDataProvider;
@@ -13,6 +14,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -107,6 +109,90 @@ public class CES_ContactPage {
 
 	@FindBy(xpath = "//button[text()='Next']")
 	WebElement nextBtn;
+	
+	@FindBy(xpath = "//input[contains(@name,'License_Number')]")
+	WebElement enterLicenseNumber;
+
+	@FindBy(xpath = "//button[contains(@name,'License_State')]")
+	WebElement licenseStateDrp;
+
+	String state = "//span[text()='%s']";
+	
+	String country="//span[text()='%s']";
+
+	@FindBy(xpath = "//input[contains(@name,'License_Date')]")
+	WebElement licenseStartDate;
+
+	@FindBy(xpath = "//button[text()='Today']")
+	WebElement selectTodayDate;
+
+	@FindBy(xpath = "//input[contains(@name,'License_Expire_Date__c')]")
+	WebElement licenseExpireDate;
+	
+	@FindBy(xpath="//button[contains(@aria-label,'Join License Country')]")
+	WebElement licenseCountryDrp;
+	
+	@FindBy(xpath = "//button[contains(@aria-label,'Subscription Plans')]")
+	WebElement selectDuesDrp;
+
+	@FindBy(xpath = "//span[contains(@title,'Payment in Full')]")
+	WebElement selectDeusOpt;
+	
+	@FindBy(xpath="//span[contains(@title,'Dues Installment Plan ')]")
+	WebElement selectPayInInsatllmentElement;
+
+	@FindBy(xpath = "//button[contains(text(),'Create sales order')]")
+	WebElement createSalesOrder;
+
+	@FindBy(xpath = "//button[text()='Ready For Payment']")
+	WebElement readyForPaymentBtn;
+
+	@FindBy(xpath = "//button[text()='Apply Payment']")
+	WebElement applyPayment;
+	
+	@FindBy(xpath = "//span[text()='Apply Payment']/parent::button")
+	WebElement applyLastPayment;
+
+	@FindBy(xpath = "//input[@name='full_name']")
+	WebElement cardHolderName;
+
+	@FindBy(xpath = "//input[@id='card_number']")
+	WebElement cardNum;
+
+	@FindBy(xpath = "//select[@aria-label='Exp month']")
+	WebElement expMonth;
+
+	@FindBy(xpath = "//select[@aria-label='Exp year']")
+	WebElement expYear;
+
+	@FindBy(xpath = "(//iframe[@title='accessibility title'])[3]")
+	WebElement drpIframe;
+
+	@FindBy(xpath = "//iframe[@title='Payment Form']")
+	WebElement cardNumIframe1;
+
+	@FindBy(xpath = "//iframe[@title='Card number']")
+	WebElement cardNumIframe2;
+
+	@FindBy(xpath = "//span[text()='Process Payment']/parent::button")
+	WebElement processPaymentBtn;
+	
+	@FindBy(xpath = "//lightning-formatted-text[@slot='primaryField']")
+	WebElement receiptNo;
+
+	@FindBy(xpath = "(//a[contains(@href,'OrderApi__Sales_Order__c')])[2]/slot/slot/span")
+	WebElement aiaNumber;
+
+	@FindBy(xpath = "(//p[text()='Total']/parent::div/p)[2]/slot/lightning-formatted-text")
+	WebElement totalAmmount;
+	
+	String contactName = "//a[text()='%s']";
+
+	@FindBy(xpath = "//a[contains(text(),'Show All (2')]")
+	WebElement showAll;
+
+
+
 
 
 
@@ -175,6 +261,11 @@ public class CES_ContactPage {
 		saveBtn.click();
 	}
 	
+	/**
+	 * @param membership
+	 * @param career
+	 * @throws InterruptedException
+	 */
 	public void joinCreatedUser(String membership, String career) throws InterruptedException {
 		Thread.sleep(8000);
 		util.waitUntilElement(driver, joinBtn);
@@ -194,6 +285,127 @@ public class CES_ContactPage {
 		executor.executeScript("arguments[0].scrollIntoView(true);", nextBtn);
 		util.waitUntilElement(driver, nextBtn);
 		nextBtn.click();
+	}
+	
+	/**
+	 * 
+	 */
+	public void enterLicenseDetail() {
+		util.enterText(driver, enterLicenseNumber, data.testDataProvider().getProperty("LICENSE_NUMBER"));
+		util.waitUntilElement(driver, licenseCountryDrp);
+		licenseCountryDrp.click();
+		executor.executeScript("arguments[0].click();", util.getCustomizedWebElement(driver, country, data.testDataProvider().getProperty("LICENSE_COUNTRY")));
+		licenseStateDrp.click();
+		WebElement enterState = driver
+				.findElement(By.xpath(String.format(state, data.testDataProvider().getProperty("LICENSE_STATE"))));
+		enterState.click();
+		action.scrollToElement(licenseStartDate);
+		licenseStartDate.click();
+		util.waitUntilElement(driver, selectTodayDate);
+		selectTodayDate.click();
+		util.enterText(driver, licenseExpireDate, data.testDataProvider().getProperty("LICENSE_EXP_DATE"));
+		licenseExpireDate.sendKeys(Keys.ENTER);
+		executor.executeScript("arguments[0].scrollIntoView(true);", nextBtn);
+		nextBtn.click();
+	}
+	
+	/**
+	 * @param null Use for loop to getting all drop-down elements select payment
+	 *             option from dropdown
+	 * @throws InterruptedException
+	 */
+	public void createSalesOrder(String paymentOpt) throws InterruptedException {
+		util.waitUntilElement(driver, selectDuesDrp);
+		selectDuesDrp.click();
+		// executor.executeScript("arguments[0].click();", selectDeusOpt);
+		selectDeusOpt.click();
+		createSalesOrder.click();
+		util.waitUntilElement(driver, readyForPaymentBtn);
+		readyForPaymentBtn.click();
+		util.waitUntilElement(driver, applyPayment);
+		applyPayment.click();
+		Thread.sleep(10000);
+		// check wait
+		driver.switchTo().frame(drpIframe);
+		Thread.sleep(60000);
+		// check wait
+		List<WebElement> options = driver.findElements(By.xpath("//select[@aria-label='Payment Type']/option"));
+		for (WebElement drpOption : options) {
+			System.out.println(drpOption.getText());
+			if (drpOption.getText().equalsIgnoreCase(paymentOpt)) {
+				drpOption.click();
+			}
+		}
+		util.waitUntilElement(driver, applyLastPayment);
+		applyLastPayment.click();
+	}
+
+	/**
+	 * @param fullName
+	 * @param null
+	 * @throws InterruptedException
+	 */
+	public void applyPayment(String fullName) throws InterruptedException {
+		util.enterText(driver, cardHolderName, fullName);
+		util.waitUntilElement(driver, cardNumIframe1);
+		driver.switchTo().frame(cardNumIframe1);
+		util.waitUntilElement(driver, cardNumIframe2);
+		driver.switchTo().frame(cardNumIframe2);
+		action.scrollToElement(cardNum);
+		util.enterText(driver, cardNum, data.testDataProvider().getProperty("CREDIT_CARD_NUMBER"));
+		driver.switchTo().defaultContent();
+		// check wait
+		Thread.sleep(5000);
+		driver.switchTo().frame(drpIframe);
+		util.waitUntilElement(driver, expMonth);
+		action.scrollToElement(expMonth);
+		util.selectDrp(expMonth).selectByValue(data.testDataProvider().getProperty("CREDIT_CARD_EXP_MONTH"));
+		util.waitUntilElement(driver, expYear);
+		util.selectDrp(expYear).selectByValue(data.testDataProvider().getProperty("CREDIT_CARD_EXP_YEAR"));
+		processPaymentBtn.click();
+	}
+
+	/**
+	 * @return
+	 * 
+	 */
+	public ArrayList<Object> getPaymentReceiptData() {
+		ArrayList<Object> receiptData = new ArrayList<Object>();
+		util.waitUntilElement(driver, receiptNo);
+		String receiptNumber = receiptNo.getText();
+		receiptData.add(0, receiptNumber);
+		util.waitUntilElement(driver, aiaNumber);
+		String customerAIANumber = aiaNumber.getText();
+		receiptData.add(1, customerAIANumber);
+		String totalAmmountText = totalAmmount.getText().replaceAll("[$]*", "").trim();
+		System.out.println(totalAmmountText);
+		receiptData.add(2, totalAmmountText);
+		return receiptData;
+	}
+	
+	/**
+	 * @param Userfullname
+	 * @throws InterruptedException
+	 * 
+	 */
+	public void selectContact(String userFullname) throws InterruptedException {
+		util.waitUntilElement(driver, contacts);
+		contactsDiv.click();
+		Thread.sleep(5000);
+		driver.navigate().refresh();
+		util.waitUntilElement(driver, tableheaderName);
+		Thread.sleep(5000);
+		util.waitUntilElement(driver, contactallBtn);
+		contactallBtn.click();
+		util.waitUntilElement(driver, contactallLink);
+		contactallLink.click();
+		Thread.sleep(14000);
+		executor.executeScript("arguments[0].scrollIntoView(true);", util.getCustomizedWebElement(driver, contactName, userFullname));
+		util.waitUntilElement(driver, util.getCustomizedWebElement(driver, contactName, userFullname));
+		executor.executeScript("arguments[0].click();",
+				util.getCustomizedWebElement(driver, contactName, userFullname));
+		util.waitUntilElement(driver, showAll);
+		showAll.click();
 	}
 
 }
