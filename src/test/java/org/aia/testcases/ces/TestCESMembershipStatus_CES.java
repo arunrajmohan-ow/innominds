2ec890181d18a767d3f1986d765d6e9bcb2911ac
Suhas <sghodake@innominds.com>
2023-09-21 14:57:23 +0530

    CES FC-164 start

---
+++ src/test/java/org/aia/testcases/ces/TestCESMembershipStatus_CES.java
@@ -0,0 +1,120 @@
+package org.aia.testcases.ces;
+
+import java.util.ArrayList;
+
+import org.aia.pages.BaseClass;
+import org.aia.pages.api.MailinatorCESAPI;
+import org.aia.pages.api.ces.JoinCESAPIValidation;
+import org.aia.pages.api.membership.FontevaConnectionSOAP;
+import org.aia.pages.ces.AdditionalProviderUser;
+import org.aia.pages.ces.AdditionalUsers;
+import org.aia.pages.ces.CheckOutPageCes;
+import org.aia.pages.ces.CloseBtnPageCes;
+import org.aia.pages.ces.FontevaCES;
+import org.aia.pages.ces.LoginPageCes;
+import org.aia.pages.ces.Organization;
+import org.aia.pages.ces.PaymentSuccessFullPageCes;
+import org.aia.pages.ces.PrimaryPointOfContact;
+import org.aia.pages.ces.ProviderStatement;
+import org.aia.pages.ces.SecondaryPointOfContact;
+import org.aia.pages.ces.SignUpPageCes;
+import org.aia.pages.ces.Subscription;
+import org.aia.pages.membership.OrderSummaryPage;
+import org.aia.pages.membership.PaymentInformation;
+import org.aia.pages.membership.PrimaryInformationPage;
+import org.aia.pages.membership.SignInPage;
+import org.aia.pages.membership.SignUpSuccess;
+import org.aia.utility.BrowserSetup;
+import org.aia.utility.ConfigDataProvider;
+import org.aia.utility.DataProviderFactory;
+import org.aia.utility.Logging;
+import org.aia.utility.Utility;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+public class TestCESMembershipStatus_CES extends BaseClass {
+	SignUpPageCes signUpPage;
+	SignInPage signInpage;
+	CloseBtnPageCes closeButtnPage;
+	MailinatorCESAPI mailinator;
+	SignUpSuccess successPage;
+	PrimaryInformationPage primaryInfoPage;
+	OrderSummaryPage orderSummaryPage;
+	PaymentInformation paymentInfoPage;
+	LoginPageCes loginPageCes;
+	PrimaryPointOfContact primarypocPage;
+	Organization organizationPage;
+	Subscription subscribePage;
+	SecondaryPointOfContact secPoc;
+	AdditionalUsers additionalUsers;
+	AdditionalProviderUser additionalProviderUser;
+	ProviderStatement providerStatement;
+	CheckOutPageCes checkOutPageCes;
+	PaymentSuccessFullPageCes paymntSuccesFullPageCes;
+	JoinCESAPIValidation apiValidation;
+	FontevaCES fontevaPage;
+
+	public ExtentReports extent;
+	public ExtentTest extentTest;
+	final static Logger logger = Logger.getLogger(TestCESMembershipStatus_CES.class);
+
+	@BeforeMethod(alwaysRun = true)
+	public void setUp() throws Exception {
+		sessionID = new FontevaConnectionSOAP();
+		driver = BrowserSetup.startApplication(driver, DataProviderFactory.getConfig().getValue("browser"),
+				DataProviderFactory.getConfig().getValue("ces_signin"));
+		util = new Utility(driver, 30);
+		testData = new ConfigDataProvider();
+		signUpPage = PageFactory.initElements(driver, SignUpPageCes.class);
+		signInpage = PageFactory.initElements(driver, SignInPage.class);
+		closeButtnPage = PageFactory.initElements(driver, CloseBtnPageCes.class);
+		mailinator = PageFactory.initElements(driver, MailinatorCESAPI.class);
+		successPage = PageFactory.initElements(driver, SignUpSuccess.class);
+		loginPageCes = PageFactory.initElements(driver, LoginPageCes.class);
+		primarypocPage = PageFactory.initElements(driver, PrimaryPointOfContact.class);
+		organizationPage = PageFactory.initElements(driver, Organization.class);
+		subscribePage = PageFactory.initElements(driver, Subscription.class);
+		secPoc = PageFactory.initElements(driver, SecondaryPointOfContact.class);
+		additionalUsers = PageFactory.initElements(driver, AdditionalUsers.class);
+		additionalProviderUser = PageFactory.initElements(driver, AdditionalProviderUser.class);
+		providerStatement = PageFactory.initElements(driver, ProviderStatement.class);
+		checkOutPageCes = PageFactory.initElements(driver, CheckOutPageCes.class);
+		paymntSuccesFullPageCes = PageFactory.initElements(driver, PaymentSuccessFullPageCes.class);
+		apiValidation = PageFactory.initElements(driver, JoinCESAPIValidation.class);
+		fontevaPage = PageFactory.initElements(driver, FontevaCES.class);
+	}
+
+	@Test(priority = 1, description = "Verify Membership status when term date for CES Membership is updated", enabled = true, groups = {
+			"Smoke" })
+	public void validateCESMembershipStatus() throws Exception {
+		//Here we create the user
+		String prefix = "Dr.";
+		String suffix = "Sr.";
+		signUpPage.clickSignUplink();
+		ArrayList<String> dataList = signUpPage.signUpData();
+		signUpPage.signUpUser();
+		mailinator.verifyEmailForAccountSetup(dataList.get(3));
+		closeButtnPage.clickCloseAfterVerification();
+		loginPageCes.loginToCes(dataList.get(5), dataList.get(6));
+		loginPageCes.checkLoginSuccess();
+		primarypocPage.enterPrimaryPocDetails(prefix, suffix, dataList.get(2));
+		String text = organizationPage.enterOrganizationDetails(dataList, "Other", "No",
+				"United States of America (+1)");
+		subscribePage.SubscriptionType(text, "Yes", null, "Non-profit");
+		secPoc.enterSecondaryPocDetails(dataList, prefix, suffix, "No", "United States of America (+1)");
+		additionalUsers.doneWithCreatingUsers();
+		providerStatement.providerStatementEnterNameDate2("FNProviderStatement");
+		checkOutPageCes.SubscriptionType(text);
+		Logging.logger.info("Total Amount is : " + paymntSuccesFullPageCes.amountPaid());
+		String reciptData = paymntSuccesFullPageCes.ClickonViewReceipt();
+		//Navigate to Fonteva side
+		driver.get(DataProviderFactory.getConfig().getValue("fontevaSessionIdUrl") + sessionID.getSessionID());
+		fontevaPage.changeTermDates(dataList.get(0) + " " + dataList.get(1));
+
+	}
+}
