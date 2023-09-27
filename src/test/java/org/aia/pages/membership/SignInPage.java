dbf1b1e1384b215a83ec609925d0193218f79e9c
Suhas <sghodake@innominds.com>
2023-09-27 12:20:14 +0530

    Wait remove from login

--- src/test/java/org/aia/pages/membership/SignInPage.java
+++ src/test/java/org/aia/pages/membership/SignInPage.java
@@ -48,7 +48,7 @@ public class SignInPage {
 		System.out.println("Email Text field displayed");
 		emailAddress.sendKeys(uname);
 		password.sendKeys(pwd);
-		Thread.sleep(1200000);
+		//Thread.sleep(1200000);
 		submitbtn.click();
 		Thread.sleep(5000);
 	}
