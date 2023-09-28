09cb7a1e1831e44cf2b3ec6477b9583eab505459
Suhas <sghodake@innominds.com>
2023-09-28 17:31:01 +0530

    Some changes in termdate API

--- src/test/java/org/aia/testcases/ces/TestRenewArchitectureEcheck_CES.java
+++ src/test/java/org/aia/testcases/ces/TestRenewArchitectureEcheck_CES.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import org.aia.pages.BaseClass;
 import org.aia.pages.api.MailinatorAPI;
 import org.aia.pages.api.MailinatorCESAPI;
+import org.aia.pages.api.ces.FontevaCESTermDateChangeAPI;
 import org.aia.pages.api.ces.FontevaConnection;
 import org.aia.pages.api.ces.FontevaConnectionSOAP;
 import org.aia.pages.api.ces.JoinCESAPIValidation;
@@ -64,6 +65,7 @@ public class TestRenewArchitectureEcheck_CES extends BaseClass {
 	RenewCESAPIValidation apiValidation;
 	FontevaCES fontevaPage;
 	RenewCESPage renew;
+	FontevaCESTermDateChangeAPI termDateChangeAPICall;
 
 	public ExtentReports extent;
 	public ExtentTest extentTest;
@@ -91,9 +93,10 @@ public class TestRenewArchitectureEcheck_CES extends BaseClass {
 		apiValidation = PageFactory.initElements(driver, RenewCESAPIValidation.class);
 		fontevaPage = PageFactory.initElements(driver, FontevaCES.class);
 		renew = PageFactory.initElements(driver, RenewCESPage.class);
+		termDateChangeAPICall = PageFactory.initElements(driver, FontevaCESTermDateChangeAPI.class);
 	}
 
-	@Test(priority = 1, description = "Renew Online Architecture Firm (E-check).", enabled = true, groups= {"Smoke"})
+	@Test(priority = 1, description = "Renew Online Architecture Firm (E-check).", enabled = false, groups= {"Smoke"})
 	public void ValidateRenewECheckArchitecture() throws Exception {
 		String prefix = "Dr.";
 		String suffix = "Sr.";
@@ -149,7 +152,8 @@ public class TestRenewArchitectureEcheck_CES extends BaseClass {
 		// Navigate to Fonteva app and make record renew eligible.
 		driver.get("https://aia--testing.sandbox.my.salesforce.com/secur/frontdoor.jsp?sid=" + sID);
 		//driver.get(DataProviderFactory.getConfig().getValue("fonteva_endpoint"));
-		fontevaPage.changeTermDates(dataList.get(0)+" "+dataList.get(1));
+		//fontevaPage.changeTermDates(dataList.get(0)+" "+dataList.get(1));
+		termDateChangeAPICall.changeTermDateAPI(dataList.get(3), "2022-12-31");
 		
 		// Navigate back to renew CES portal
 		driver.get("https://account-dev.aia.org/signin?redirectUrl=https%3A%2F%2Faia--testing.sandbox.my.site.com%2FProviders%2Fs%2Frenew");
