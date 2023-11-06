c3d2523d9bd91ecf4c284d78ca918403c34f969f
sghodake <sghodake@IM-LP-2521.innomindshyd.com>
2023-11-06 19:53:57 +0530

    New

--- src/test/java/org/aia/utility/Utility.java
+++ src/test/java/org/aia/utility/Utility.java
@@ -14,6 +14,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -380,7 +381,10 @@ public class Utility {
 	* Here we are using awaitility for waiting the response from api
 	*/
 	public void waitForResponse(final Response response, final int statusCode) {
-      Awaitility.await().atMost(10,TimeUnit.SECONDS).until(()->{return response.getStatusCode()==statusCode;});
+      Awaitility.await().atMost(10,TimeUnit.SECONDS).until(new Callable<Boolean>() {
+		@Override
+		public Boolean call() throws Exception {return response.getStatusCode()==statusCode;}
+	});
 	}
 
 }
